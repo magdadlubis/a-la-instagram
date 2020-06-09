8fea176280c06665ee87923f298276e81e5f42f6
Magdalena Dłubis <magdadlubis@gmail.com>
2020-06-09 22:33:49 +0200

    Zadanie 3 cz.1

--- photoalbum/models.py
+++ photoalbum/models.py
@@ -1,3 +1,11 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
 # Create your models here.
+
+UserModel = get_user_model()
+
+class Photo(models.Model):
+    path = models.CharField(max_length=128, verbose_name='Ścieżka do zdjęcia')
+    creation_date = models.DateTimeField(auto_now_add=True, verbose_name='Data dodania')
+    user = models.ForeignKey(UserModel, on_delete=models.CASCADE, verbose_name='Użytkownik')
